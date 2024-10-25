0f2d1fc36af8ec91c9bcc03f30759d87b96320c6
moss0 <2317919@students.ucreative.ac.uk>
2024-10-25 12:53:27 +0100

    Modifying the jump mechanics.

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,21 @@ public class PlayerController : MonoBehaviour
     public float playerSpeed = 15f, jumpForce = 30f;
 
     private Rigidbody _rb;
-    
-    private bool _isGrounded;
+
+    private bool _isGrounded, _jumpTimerExceeded;
+    private float _jumpTimer = 0, _jumpTimerMax;
     private Camera _mainCamera;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
         _mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (_jumpTimer > _jumpTimerMax)
+        _jumpTimer += Time.deltaTime;
     }
 
     private void FixedUpdate()
@@ -27,10 +34,16 @@ public class PlayerController : MonoBehaviour
         _rb.AddForce(movement * playerSpeed);
 
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.8f)) _isGrounded = true;
-        else _isGrounded = false;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.51f))
+        {
+            _isGrounded = true;
+        }
+        else
+        {
+            _isGrounded = false;
+        }
 
-        if (Input.GetKey("space") && _isGrounded)
+        if (_isGrounded && !_jumpTimerExceeded)
         {
             Vector3 jump = new Vector3(0.0f, (jumpForce * 10), 0.0f);
             _rb.AddForce(jump);
