3583db08f7ea087c66e13ebfc71d6a926b2b3bf3
moss0 <2317919@students.ucreative.ac.uk>
2024-10-25 16:59:24 +0100

    Constant jumping added.

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -7,30 +7,34 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody _rb;
 
-    private bool _isGrounded, _jumpTimerExceeded;
-    private float _jumpTimer = 0, _jumpTimerMax;
+    [SerializeField] private bool _isGrounded;
+    private float _timer;
     private Camera _mainCamera;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
         _mainCamera = Camera.main;
+        Time.timeScale = 0.3f;
     }
 
     private void Update()
     {
-        if (_jumpTimer > _jumpTimerMax)
-        _jumpTimer += Time.deltaTime;
+        if (!_isGrounded && _rb.velocity.y == 0)
+        {
+            //Use this: Mathf.Approximately(1.0f, 10.0f / 10.0f)
+            Debug.Log("Height = " + _rb.transform.position.y);
+        }
     }
 
     private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
+        
         Vector3 cameraForward = Vector3.Scale(_mainCamera.transform.forward, new Vector3(1, 0, 1)).normalized;
         Vector3 movement = (cameraForward * moveVertical + _mainCamera.transform.right * moveHorizontal).normalized;
-
+        
         _rb.AddForce(movement * playerSpeed);
 
         RaycastHit hit;
@@ -42,12 +46,11 @@ public class PlayerController : MonoBehaviour
         {
             _isGrounded = false;
         }
-
-        if (_isGrounded && !_jumpTimerExceeded)
+        
+        if (_isGrounded && _rb.velocity.y == 0)
         {
             Vector3 jump = new Vector3(0.0f, (jumpForce * 10), 0.0f);
             _rb.AddForce(jump);
-            _isGrounded = false;
         }
     }
 }
